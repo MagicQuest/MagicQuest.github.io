a4c88bf14a0460dc9075f1116c6132d508b050af
MagicQuest <61136336+MagicQuest@users.noreply.github.com>
2020-07-14 12:53:22 -0500

    adding a kanji option and a selfupdating.py file

---
+++ selfupdating.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+
+link = "https://magicquest.github.io/selfupdating.js"
+
+f = urlopen(link)
+myfile = f.read()
+#print(myfile)
+with open('selfupdating.py','r+') as this:
+    thisFile = this.read()
+    #print(thisFile)
+    if(thisFile != myfile):
+        print('hey its not the same!!')
+        print('updating!')
+        with open('selfupdating.py','w+') as this:
+            this.write(myfile.decode("utf-8"))
+            this.close()
+    else:
+        print('hey no need to update!!!')
+    this.close()
+f.close()
